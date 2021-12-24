43cc40e5666bbabcf2e4d82c8fdeb319c1a2723d
ITExpert0228 <dhv.techcenter@gmail.com>
2021-12-25 04:35:05 +0700

    Arksong update, fix styles, design input number

--- src/view/MintPage.jsx
+++ src/view/MintPage.jsx
@@ -4,6 +4,9 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import 'react-toastify/dist/ReactToastify.css';
 
 import sample4 from '../assets/images/gallery/4.png';
+import WalletModal from '../components/walletmodal/walletmodal';
+import {handleSignificantDecimals, convertAmountFromRawNumber} from '../components/walletmodal/helpers/bignumber';
+
 toast.configure()
 
 class MintPage extends Component {
@@ -14,17 +17,51 @@ class MintPage extends Component {
             value: '0x12434343434333',
             copied: false,
             count: 5,
-            connected: false
+            connected: false,
+            address: '',
+            symbol: '',
+            balance: '',
+            network: '',
+            web3: null
         };
 
         this.countHandler.bind(this);
         this.tooltipHandler.bind(this);
+        this.getWeb3Details.bind(this);
+        this.incrementValue.bind(this);
+        this.decrementValue.bind(this);
     }
 
     countHandler(e) {
         this.setState({ count: e.target.value });
     }
-
+    incrementValue(e) {
+        e.preventDefault();
+       
+        let currentVal = this.state.count;
+        
+        if (currentVal >= 1) {
+            //parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+            this.setState({ count:  1 + Number(currentVal) });
+        } else {
+            //parent.find('input[name=' + fieldName + ']').val(0);
+            this.setState({ count: 1 });
+        }
+    }
+    
+    decrementValue(e) {
+        e.preventDefault();
+        let currentVal = this.state.count;
+      
+        if (currentVal > 1) {
+          //parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+          this.setState({ count: currentVal - 1 });
+        } else {
+          //parent.find('input[name=' + fieldName + ']').val(0);
+          this.setState({ count: 1 });
+        }
+      }
+      
     tooltipHandler(e) {
         e.preventDefault();
         let tooltipNode = e.currentTarget.querySelector('.popuptext');
@@ -33,6 +70,19 @@ class MintPage extends Component {
         setTimeout(() => {
             tooltipNode.classList.remove('show');
         }, 2000);
+    }
+
+    getWeb3Details(param) {
+        const _symbol = param._balance.length > 0 ? param._balance[0].symbol : '';
+        const _balance = param._balance.length > 0 ? `${handleSignificantDecimals(convertAmountFromRawNumber(param._balance[0].balance), 8)}` : '';
+        console.log(param._web3);
+
+        this.setState({
+            address: param._address, 
+            network: param._network,
+            symbol: _symbol,
+            balance: _balance,
+            web3: param._web3});
     }
 
     render() {
@@ -75,7 +125,7 @@ class MintPage extends Component {
                             <h1 className="fw-800 f-72 text-white border-bottom border-bottm-width-7">CURRENT : {data.current}</h1>
                             <h2 className="fw-800 f-40 text-white mb-0 pd-b10">Price: ${data.price} each</h2>
                             <h2 className="fw-800 f-40 text-white mb-0 pd-b10">Target Supply: {data.targetSupply} NFT's</h2>
-                            <h2 className="fw-800 f-40 text-white pd-b140 d-flex align-items-center address">
+                            <h2 className="fw-800 f-40 text-white d-flex align-items-center address">
                                 <span className="d-flex align-items-center flex-md-row flex-column">Contract Address:
                                     <span className='ml-2'>
                                         {data.contactAddress}
@@ -91,8 +141,23 @@ class MintPage extends Component {
                             </h2>
 
                             <div className="connection-group d-flex align-items-center justify-content-center pd-b60">
-                                <a href="#" className="btn btn-gradient btn-type-one mr-4 d-flex align-items-center justify-content-center" onClick={e => { e.preventDefault(); this.setState({ connected: true }); }}>{this.state.connected ? 'CONNECTED' : 'CONNECT'}</a>
-                                <input type="number" min="1" max="25" dir="rtl" className="form-control" placeholder="Enter Here" onChange={(e) => { this.countHandler(e); }} value={this.state.count} />
+                               
+                                {/* <input type="text" className="form-control" placeholder="Address" value={this.state.address} readOnly/>
+                                <input type="text" className="form-control" placeholder="ChainId" value={this.state.network} readOnly/>
+                                <input type="text" className="form-control" placeholder="Balance" value={this.state.balance + this.state.symbol} readOnly/> */}
+                            </div>
+
+                            <div className="connection-group d-flex align-items-center justify-content-center pd-b60">
+                                <WalletModal getInfoCallback={param => this.getWeb3Details(param)} />
+                                
+                                {/* <input type="number" min="1" max="25" className="form-control" placeholder="Enter Here" onChange={(e) => { this.countHandler(e); }} value={this.state.count} /> */}
+                                
+                                <div className="input-group">
+                                    <input type="button" value="-" className="button-minus" data-field="quantity" onClick={(e) => {this.decrementValue(e)}} />
+                                    <input type="number" step="1" max="" value="1" name="quantity" className="quantity-field"  onChange={(e) => { this.countHandler(e); }} value={this.state.count} />
+                                    <input type="button" value="+" className="button-plus" data-field="quantity" onClick={(e) => {this.incrementValue(e)}} />
+                                </div>
+
                                 <a href="#" className="btn btn-gradient btn-type-one ml-4 d-flex align-items-center justify-content-center" onClick={e => { e.preventDefault(); notify(); }}>MINT</a>
                             </div>
 
